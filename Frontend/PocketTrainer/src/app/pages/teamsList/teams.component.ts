5d88af9c3570bc4bd78fdc5d74d41553c79940db
Iván Castells Galindo <icastellsg01@gmail.com>
2023-05-31 23:26:36 +0200

    Eliminación de equipos

--- Frontend/PocketTrainer/src/app/pages/teamsList/teams.component.ts
+++ Frontend/PocketTrainer/src/app/pages/teamsList/teams.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Team } from 'src/app/interfaces/Teams';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteTeamComponent } from 'src/app/components/delete-team/delete-team.component';
+import { Team, TeamObserver } from 'src/app/interfaces/Teams';
 import { TeamService } from 'src/app/services/team.service';
 
 @Component({
@@ -7,16 +9,36 @@ import { TeamService } from 'src/app/services/team.service';
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.scss']
 })
-export class TeamsComponent {
+export class TeamsComponent implements TeamObserver{
 
   teams: Team[] = []
   filteredTeams: Team[] = []
 
-  constructor(private teamApi: TeamService) {}
+  constructor(private teamApi: TeamService, public dialog: MatDialog) {}
+  update(teams: Team[]): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit() {
     this.teamApi.getTeams().subscribe((result) => {
       this.teams = result;
     });
-}
+  }
+
+  openDialog(event: MouseEvent, team: Team): void {
+    event.preventDefault();
+    const dialogRef = this.dialog.open(DeleteTeamComponent, {
+      data: team.name
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result){
+        this.teamApi.deleteTeam(Number(team.id)).subscribe({
+          next: (result) => {
+            console.log(result)
+        }
+        })
+      }
+    });
+  }
 }
