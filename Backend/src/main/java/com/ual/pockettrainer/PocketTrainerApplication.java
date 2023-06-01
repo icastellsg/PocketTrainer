f1e776095fe3140fa878b12bbe82f2399680d377
Iván Castells Galindo <icastellsg01@gmail.com>
2023-06-01 20:53:59 +0200

    bugFixing Cors

--- Backend/src/main/java/com/ual/pockettrainer/PocketTrainerApplication.java
+++ Backend/src/main/java/com/ual/pockettrainer/PocketTrainerApplication.java
@@ -17,7 +17,7 @@ public class PocketTrainerApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200",
-				"http://localhost:8080", "http://34.125.57.167", "http://34.125.57.167:8081",
+				"http://localhost:8080", "http://34.125.57.167:443", "http://34.125.57.167:8081",
 				"https://ualdra.github.io"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
 				"Access-Control-Allow-Origin", "Content-Type",
